7f941aab0c4313226aaf83de59d79900526b94fb
Itan Ortega <io@MacBook-Air-de-Itan.local>
2018-04-21 11:52:01 -0500

    Conexión a api

--- app/app/src/main/java/com/sime/demoweather/CAFData.java
+++ app/app/src/main/java/com/sime/demoweather/CAFData.java
@@ -1,5 +1,17 @@
+//
+//  CAFData.java
+//
+//  Created by Cesar Franco on 10/07/16.
+//  Version 1.0 Alpha 1
+//  Copyright © 2015 Cesar Franco. All rights reserved.
+//
+
 package com.sime.demoweather;
 
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Log;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
@@ -7,22 +19,27 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
 import java.nio.ByteBuffer;
 import java.nio.charset.Charset;
+import java.util.Date;
+import java.util.Map;
 
 public class CAFData {
-
     private static final int bufferSize = 100 * 1024; // ~130K.
     private ByteBuffer byteData = null;
 
+
     //Constructors
+
     public CAFData(byte[] bytes, int length) {
 
         byteData = ByteBuffer.wrap(bytes, 0, length);
     }
+
 
     public CAFData(String path) {
         File inFile = new File(path);
@@ -41,6 +58,44 @@ public class CAFData {
 
     }
 
+    public CAFData(URL url, String args) {
+        InputStream inStream = null;
+        int response = -1;
+        URLConnection conn = null;
+
+        try {
+            conn = url.openConnection();
+            if (conn instanceof HttpURLConnection) {
+                HttpURLConnection httpConn = (HttpURLConnection) conn;
+                httpConn.setRequestMethod("POST");
+                httpConn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+                httpConn.setReadTimeout(30000);
+                httpConn.setConnectTimeout(15000);
+                httpConn.setAllowUserInteraction(false);
+                httpConn.setInstanceFollowRedirects(true);
+                httpConn.setDoInput(true);
+
+                OutputStream os = conn.getOutputStream();
+                os.write(args.getBytes());
+                os.flush();
+
+                response = httpConn.getResponseCode();
+
+                if (response == HttpURLConnection.HTTP_OK) {
+                    inStream = httpConn.getInputStream();
+                    byteData = getBytesFromInputStream(inStream);
+                    inStream.close();
+                }
+                httpConn.disconnect();
+            }
+
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+
     public CAFData(URL url) {
         InputStream inStream = null;
         int response = -1;
@@ -50,9 +105,12 @@ public class CAFData {
             conn = url.openConnection();
             if (conn instanceof HttpURLConnection) {
                 HttpURLConnection httpConn = (HttpURLConnection) conn;
+                httpConn.setReadTimeout(10000);
+                httpConn.setConnectTimeout(15000);
                 httpConn.setAllowUserInteraction(false);
                 httpConn.setInstanceFollowRedirects(true);
                 httpConn.setRequestMethod("GET");
+                httpConn.setDoInput(true);
                 httpConn.connect();
                 response = httpConn.getResponseCode();
                 if (response == HttpURLConnection.HTTP_OK) {
@@ -61,6 +119,7 @@ public class CAFData {
                     byteData = getBytesFromInputStream(inStream);
                     inStream.close();
                     //} catch (){
+
 
                     //}
                 }
@@ -73,8 +132,10 @@ public class CAFData {
         }
     }
 
+
     // Private methods
-    private static ByteBuffer getBytesFromInputStream(InputStream inStream) {
+
+    protected static ByteBuffer getBytesFromInputStream(InputStream inStream) {
         byte[] buffer = new byte[bufferSize];
         ByteArrayOutputStream outStream = null;
         ByteBuffer byteData = null;
@@ -84,9 +145,8 @@ public class CAFData {
             int bytesRead = 0;
             while (bytesRead != -1) {
                 bytesRead = inStream.read(buffer);
-                if (bytesRead > 0) {
+                if (bytesRead > 0)
                     outStream.write(buffer, 0, bytesRead);
-                }
             }
             byteData = ByteBuffer.wrap(outStream.toByteArray());
         } catch (IOException e) {
@@ -98,13 +158,23 @@ public class CAFData {
         return byteData;
     }
 
+
     // Public methods
+
+    public static CAFData dataWithContentsOfURL(URL url, String args) {
+
+        CAFData data = new CAFData(url, args);
+        if (data.length() == 0)
+            data = null;
+
+        return data;
+    }
+
     public static CAFData dataWithContentsOfURL(URL url) {
 
         CAFData data = new CAFData(url);
-        if (data != null && data.length() == 0) {
+        if (data != null && data.length() == 0)
             data = null;
-        }
 
         return data;
     }
@@ -112,11 +182,21 @@ public class CAFData {
     public static CAFData dataWithContentsOfFile(String fullFilename) {
 
         CAFData data = new CAFData(fullFilename);
-        if (data != null && data.length() == 0) {
+        if (data != null && data.length() == 0)
             data = null;
-        }
 
         return data;
+    }
+
+    public Bitmap toImage() {
+        Bitmap img = null;
+        byte[] bytes = byteData.array();
+        try {
+            img = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+        } catch (Exception e) {
+            img = null;
+        }
+        return img;
     }
 
     // Write content in local file
@@ -129,22 +209,21 @@ public class CAFData {
             if (atomically) {
                 inFile = File.createTempFile("tmp", ".tmp");
                 fis = new FileOutputStream(inFile);
-            } else {
+            } else
                 inFile = new File(path);
-            }
 
             fis.write(byteData.array());
-            //Log.d("CAFDataEx",inFile.getName()+" ("+byteData.array().length+" bytes) saved.");
+            Log.d("CAFData", inFile.getName() + " (" + byteData.array().length + " bytes) saved.");
+            boolean b = inFile.setLastModified(new Date().getTime());
             fis.close();
 
             if (atomically) {
-                if (inFile.renameTo(new File(path))) {
-                    //Log.d("CAFDataEx","Renamed to "+path);
-                }
+                if (inFile.renameTo(new File(path)))
+                    Log.d("CAFData", "Renamed to " + path);
 
             }
 
-            //Log.d("CAFDataEx","Bytes writed = "+byteData.array().length);
+            //Log.d("CAFData","Bytes writed = "+byteData.array().length);
             wasWritten = true;
         } catch (FileNotFoundException e1) {
             // TODO Auto-generated catch block
@@ -161,13 +240,17 @@ public class CAFData {
         return new String(byteData.array(), Charset.forName("UTF-8"));
     }
 
+
     public int length() {
         int length = 0;
 
-        if (byteData != null) {
+        if (byteData != null)
             length = byteData.array().length;
-        }
 
         return length;
+    }
+
+    public boolean isEqualToData(CAFData other) {
+        return byteData.equals(other.byteData);
     }
 }
--- app/app/src/main/java/com/sime/demoweather/MainActivity.java
+++ app/app/src/main/java/com/sime/demoweather/MainActivity.java
@@ -2,16 +2,33 @@ package com.sime.demoweather;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText Txt_Query;
-    Button Btn_Search;
-    EditText Txt_Current;
-    EditText Txt_Min;
-    EditText Txt_Max;
+    private ExecutorService queue = Executors.newSingleThreadExecutor();
+
+    private final static String KEY = "96d1e8ee17719778cb471ad2f4c85ae2";
+    private final static String DOMAIN = "https://api.openweathermap.org/data/2.5/weather";
+
+    private EditText Txt_Query;
+    private Button Btn_Search;
+    private TextView Lbl_Current;
+    private TextView Lbl_Min;
+    private TextView Lbl_Max;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,8 +37,60 @@ public class MainActivity extends AppCompatActivity {
 
         Txt_Query = (EditText) this.findViewById(R.id.Txt_Query);
         Btn_Search = (Button) this.findViewById(R.id.Btn_Search);
-        Txt_Current = (EditText) this.findViewById(R.id.Txt_Current);
-        Txt_Min = (EditText) this.findViewById(R.id.Txt_Min);
-        Txt_Max = (EditText) this.findViewById(R.id.Txt_Max);
+        Lbl_Current = (TextView) this.findViewById(R.id.Lbl_Current);
+        Lbl_Min = (TextView) this.findViewById(R.id.Lbl_Min);
+        Lbl_Max = (TextView) this.findViewById(R.id.Lbl_Max);
+
+        Btn_Search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String query = Txt_Query.getText().toString();
+                search(query);
+            }
+        });
+    }
+
+    public void search(String query){
+        final String queryTmp = query;
+
+        Runnable thread = new Runnable() {
+            @Override
+            public void run() {
+                String strUrl = DOMAIN + "?q=" + queryTmp + "&appid=" + KEY;
+                URL url = null;
+                CAFData remoteData = null;
+
+                try {
+                    url = new URL(strUrl);
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
+
+                if(url != null){
+                    remoteData = CAFData.dataWithContentsOfURL(url);
+
+                    try {
+                        JSONObject root = new JSONObject(remoteData.toText());
+                        JSONObject main = root.getJSONObject("main");
+                        final float temp = (float) main.getDouble("temp");
+                        final float temp_min = (float) main.getDouble("temp_min");
+                        final float temp_max = (float) main.getDouble("temp_max");
+
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Lbl_Current.setText(String.valueOf(temp));
+                                Lbl_Min.setText(String.valueOf(temp_min));
+                                Lbl_Max.setText(String.valueOf(temp_max));
+                            }
+                        });
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+        queue.execute(thread);
     }
 }
